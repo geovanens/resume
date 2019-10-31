bd78d601c027aebd01c7560f37a7fb2ae83da595
geovanens <geovane.silva@ccc.ufcg.edu.br>
2019-10-31 20:13:02 -0300

    format css

--- resume/views.py
+++ resume/views.py
@@ -8,25 +8,28 @@ from .models import Perfil, Profissional, Academico
 # Create your views here.
 
 def index(request):
-    obj = Perfil.objects.all()[0]
-    context = {'data': obj}
-    print("Index", context['data'].foto)
+    foto = Perfil.objects.all()[0].foto
+    context = {'foto': foto}
     return render(request, 'resume/index.html', context)
 
 def perfil(request):
+    print(request)
     obj = Perfil.objects.all()[0]
+    foto = obj.foto
     hoje = datetime.now()
     formatado = date(hoje.year, hoje.month, hoje.day)
     idade = abs(formatado - obj.nascimento).days//365
-    context = {'data': obj, 'idade': idade}
+    context = {'data': obj, 'idade': idade, 'foto': foto}
     return render(request, 'resume/perfil.html', context)
 
 def profissional(request):
+    foto = Perfil.objects.all()[0].foto
     obj = Profissional.objects.all()
-    context = {'data': obj}
+    context = {'data': obj, 'foto': foto}
     return render(request, 'resume/profissional.html', context)
 
 def academico(request):
     obj = Academico.objects.all()
-    context = {'data': obj}
+    foto = Perfil.objects.all()[0].foto
+    context = {'data': obj, 'foto': foto}
     return render(request, 'resume/academico.html', context)
